80fdb8ab222af75e3fc8784e4cc71bde400f2688
UnkindledTwo <dinikez22@gmail.com>
2021-07-21 11:16:10 +0300

    Bug fixes

--- treenotes.cpp
+++ treenotes.cpp
@@ -302,14 +302,14 @@ void TreeNotes::ReadChildren(QDomDocument *doc, QDomNode node, TreeWidgetItem *p
     QDomNodeList itemList = node.toElement().elementsByTagName("NoteItem");
     for(int i = 0; i < node.toElement().childNodes().count(); i++){
         QDomElement currentElement = node.toElement().childNodes().at(i).toElement();
-        TreeWidgetItem *newItem = AddNote(parent, "");
+        TreeWidgetItem *newItem = new TreeWidgetItem();
 
         newItem->setIcon(0 , iconVector.at(currentElement.attribute("icon").toInt()));
         newItem->iconVectorIndex = currentElement.attribute("icon").toInt();
         newItem->lastEdited = QDateTime::fromString(currentElement.attribute("lastEdited"));
         newItem->starred = qvariant_cast<bool>(currentElement.attribute("starred", "0"));
         newItem->readOnly = qvariant_cast<bool>(currentElement.attribute("readOnly", "0"));
-        //parent->addChild(newItem);
+        parent->addChild(newItem);
         setStar(newItem, newItem->starred);
         ReadChildren(doc, node.toElement().childNodes().at(i), newItem);
     }
@@ -351,13 +351,13 @@ void TreeNotes::ReadFromFile(){
             if(itemList.at(i).toElement().attribute("title") == "") continue;
 
             QDomElement currentElement = root.toElement().childNodes().at(i).toElement();
-            TreeWidgetItem *newItem = AddNote(NULL, "");
+            TreeWidgetItem *newItem = new TreeWidgetItem();
             newItem->setIcon(0 , iconVector.at(currentElement.attribute("icon").toInt()));
             newItem->iconVectorIndex = (currentElement.attribute("icon").toInt());
             newItem->lastEdited = QDateTime::fromString(currentElement.attribute("lastEdited"));
             newItem->starred = qvariant_cast<bool>(currentElement.attribute("starred", "0"));
             newItem->readOnly = qvariant_cast<bool>(currentElement.attribute("readOnly", "0"));
-            //noteTree->addTopLevelItem(newItem);
+            noteTree->addTopLevelItem(newItem);
             setStar(newItem, newItem->starred);
             ReadChildren(&document, root.toElement().childNodes().at(i), newItem);
         }
